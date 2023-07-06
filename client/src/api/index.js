4d72b3376a1869b48233cca13737de032e33a376
codesquadht-l1 <linx2463@gmail.com>
2023-07-06 00:34:28 -0400

    deploying

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // this the url pointing to the backend
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+// const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://pinverse.onrender.com' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
