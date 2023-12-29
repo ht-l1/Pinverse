fd4106f47cd4eb489363c9ccd8cef85988989091
Hannah Lin <ht.techx@gmail.com>
2023-12-29 15:20:55 -0500

    bugfix: update server cors setup

--- server/index.js
+++ server/index.js
@@ -13,7 +13,15 @@ const app = express();
 
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: 'Content-Type,Authorization',
+}));
+
+app.options('*', cors());
 
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
