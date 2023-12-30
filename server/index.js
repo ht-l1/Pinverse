572aa1a949a8673e64d724e6d743817efd6820fc
Hannah Lin <ht.techx@gmail.com>
2023-12-30 12:00:35 -0500

    configure cors

--- server/index.js
+++ server/index.js
@@ -13,15 +13,16 @@ const app = express();
 
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
-// app.use(cors());
-app.use(cors({
-    origin: 'http://localhost:3000',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    allowedHeaders: 'Content-Type,Authorization',
-}));
+app.use(cors());
+// app.use(cors({
+//     origin: 'https://pinverse.netlify.app',
+// origin: 'http://localhost:3000',
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true,
+//     allowedHeaders: 'Content-Type,Authorization',
+// }));
 
-app.options('*', cors());
+// app.options('*', cors());
 
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
